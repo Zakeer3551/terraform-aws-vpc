5cf617675826451242e1f8709725ce921cfb655f
zakeer3551 <zak.ican4d@gmail.com>
2026-02-27 23:56:13 +0530

    developing and practising vpc module

--- locals.tf
+++ locals.tf
@@ -16,4 +16,13 @@ locals {
   )
 
   availability_zone =  slice(data.aws_availability_zones.azs_info.names, 0, 2)
+
+  public_subnet_final_tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    },
+    var.public_subnet_tags
+
+  )
 }
